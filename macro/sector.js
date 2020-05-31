f1f383955a5413353f254da6b718880f72dccefb
Nick Gibbins <nick@gark.net>
2020-05-31 19:49:48 +0100

    Refactored sector/subsector mapping code to remove overuse of TiddlyWiki templates.

---
+++ macro/sector.js
@@ -0,0 +1,78 @@
+/*\
+created: 20200531102740734
+title: $:/plugins/nickgark/AAB/macro/sector
+type: application/javascript
+modified: 20200531102753034
+tags: 
+module-type: macro
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/* Tells TiddlyWiki the name of our macro through the export mechanism. */
+exports.name = "sector";
+
+/* Lists of macro parameters; leave this array empty if you need none, or
+ * want all supplied macro call parameters to be passed to the run() method.
+ */
+exports.params = [
+    {}
+];
+
+var trav = require("$:/plugins/nickgark/AAB/lib/library.js");
+var svg = require("$:/plugins/nickgark/AAB/lib/svg.js");
+var map = require("$:/plugins/nickgark/AAB/lib/map.js");
+
+/* Executes (runs) our macro when it requires evaluation; returns a string
+ * value.
+ */
+exports.run = function() {
+    var sector = new trav.Sector(this.getVariable("currentTiddler"), this.wiki);
+    var hexwidth = 25;
+    var width = hexwidth * 24.25;
+    var hexheight = 21.5;
+    var height = hexheight * 40.5;
+    var x,y,link;
+
+    // create SVG root
+    var mapsvg = svg.SVG({"viewBox":"-30 -30 " + (width+60) + 
+                                           " " + (height+60)});
+
+    // create clippath
+    var defs = svg.Defs().addTo(mapsvg);
+    var clip = Math.random();
+    var clippath = svg.ClipPath({"id":clip}).addTo(defs);
+    svg.Rect(0,0,width,height).addTo(clippath);
+    
+    // create frame          
+    map.renderFrame(sector,hexwidth,hexheight).addTo(mapsvg);
+
+    // create subsector grid  
+    svg.Path(width*0.25,0,{"stroke":"black","stroke-width":2}).L(width*0.25, height).addTo(mapsvg);
+    svg.Path(width*0.5,0,{"stroke":"black","stroke-width":2}).L(width*0.5, height).addTo(mapsvg);
+    svg.Path(width*0.75,0,{"stroke":"black","stroke-width":2}).L(width*0.75, height).addTo(mapsvg);
+    svg.Path(0,height*0.25,{"stroke":"black","stroke-width":2}).L(width, height*0.25).addTo(mapsvg);
+    svg.Path(0,height*0.5,{"stroke":"black","stroke-width":2}).L(width, height*0.5).addTo(mapsvg);
+    svg.Path(0,height*0.75,{"stroke":"black","stroke-width":2}).L(width, height*0.75).addTo(mapsvg);
+  
+    // get the subsectors that are to be displayed 
+    var subsectors = sector.getSubsectors();
+
+    subsectors.forEach(function(e) {
+        x = ((e.ssx * 0.25) + 0.125) * width;
+        y = ((e.ssy * 0.25) + 0.125) * height;
+        link = svg.A("#" + e.title).addTo(mapsvg);
+        svg.Text(e.name,{"x":x,"y":y,
+                         "transform":"rotate(-60 " + x + " " + y + ")",
+                         "text-anchor":"middle",
+                         "font-size":30,
+                         "fill":"grey"}).addTo(link);
+    });
+ 
+    return mapsvg.toString();
+};
+
+})();
---
+++ macro/subsector.js
@@ -0,0 +1,118 @@
+/*\
+created: 20200528131240246
+type: application/javascript
+title: $:/plugins/nickgark/AAB/macro/subsector
+tags: 
+modified: 20200528212726073
+module-type: macro
+\*/
+(function() {
+
+    /*jslint node: true, browser: true */
+    /*global $tw: false */
+    "use strict";
+
+    /* Tells TiddlyWiki the name of our macro through the export mechanism. */
+    exports.name = "subsector";
+
+    /* Lists of macro parameters; leave this array empty if you need none, or
+     * want all supplied macro call parameters to be passed to the run() method.
+     */
+    exports.params = [{
+    }];
+
+    var trav = require("$:/plugins/nickgark/AAB/lib/library.js");
+    var svg = require("$:/plugins/nickgark/AAB/lib/svg.js");
+    var map = require("$:/plugins/nickgark/AAB/lib/map.js");
+
+    /* Executes (runs) our macro when it requires evaluation; returns a string
+     * value.
+     */
+    exports.run = function() {
+        try {
+            var subsector = new trav.Subsector(this.getVariable("currentTiddler"), this.wiki);
+            
+            var hexwidth = 100;
+            var width = hexwidth * 6.25;
+            var hexheight = 86;
+            var height = hexheight * 10.5;
+            var x,y,hex,g;
+
+            // create SVG root
+            var mapsvg = svg.SVG({"viewBox":"-30 -30 " + (width+60) + 
+                                                   " " + (height+60)});
+
+            // create clippath
+            var defs = svg.Defs().addTo(mapsvg);
+            var clip = Math.random().toString(36).substr(2,5);
+            var clippath = svg.ClipPath({"id":clip}).addTo(defs);
+            svg.Rect(0,0,width,height).addTo(clippath);
+            
+            // create frame          
+            map.renderFrame(subsector,hexwidth,hexheight).addTo(mapsvg);
+
+            // get the systems that are to be displayed 
+            var systems = subsector.getSystems();
+
+            // build dictionary of systems by hex
+            var byHex = {};
+            systems.forEach(function(e) {
+                byHex[e.hex] = e;
+            });
+
+            for (x = 1; x <= 8; x++) {
+                for (y = 1; y <= 10; y++) {
+                    hex = trav.getHex(x + subsector.offsetx, 
+                                      y + subsector.offsety);
+                    g = svg.G().translate(trav.offsetXCentre(x, y, hexwidth),
+                                          trav.offsetYCentre(x, y, hexwidth))
+                               .addTo(mapsvg);
+                    if (byHex[hex] !== undefined) {
+                        map.renderBackground(byHex[hex]).addTo(g);
+                    } 
+                    map.renderHex().addTo(g);                  
+                }
+            }
+
+            // iterate over all systems in subsector
+            
+            g = svg.G({"clip-path":"url(#" + clip + ")"}).addTo(mapsvg);
+            
+            systems.forEach(function(start) {
+                if (start.routes != "") {
+                    start.routes.split(" ").forEach(function(end) {
+                        map.renderRoute(start.hex,end,
+                                    subsector.letter,hexwidth).addTo(g);
+                    });
+                }
+            });
+
+            // iterate over all hexes in subsector
+            for (x = 1; x <= 8; x++) {
+                for (y = 1; y <= 10; y++) {       
+                    hex = trav.getHex(x + subsector.offsetx, 
+                                      y + subsector.offsety);
+                    g = svg.G().translate(trav.offsetXCentre(x, y, hexwidth),
+                                          trav.offsetYCentre(x, y, hexwidth))
+                               .addTo(mapsvg);
+                    // look for the current hex in the dictionary              
+                    if (byHex[hex] === undefined) {
+                        // if it doesn't exist, render an empty hex
+                        map.renderEmpty(hex).addTo(g);                     
+                    } else {
+                        // if it does, render the system details                   
+                        map.renderDetails(byHex[hex]).addTo(g);
+                    }
+                }
+            }
+             
+            return mapsvg.toString();
+             
+        } catch(e) {
+            console.error(e);
+            
+            return e;
+        }
+    };
+
+})();
